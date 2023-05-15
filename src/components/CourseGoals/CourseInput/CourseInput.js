8783e4ab01c61b839c085e42a6bb55efec051599
svenku <sven.kunsing@gmail.com>
2023-05-15 21:13:53 +0300

    One way of setting classes on styled-component

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,42 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
 import "./CourseInput.css";
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+& label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+& input {
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  font: inherit;
+  line-height: 1.5rem;
+  padding: 0 0.25rem;
+}
+
+& input:focus {
+  outline: none;
+  background: #e1d5dd;
+  border-color: #8b005d;
+}
+
+&.invalid input {
+  border-color: red;
+  background: #fad0ec;
+}
+
+&.invalid label {
+  color: red;
+}
+`
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -28,13 +63,13 @@ const CourseInput = (props) => {
   // notice usage of template literal (backticks...) to inject invalid class dynamically
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      <FormControl className = {!isValid && 'invalid'}>
         <label>Course Goal</label>
         <input
           type="text"
           onChange={goalInputChangeHandler}
         />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
